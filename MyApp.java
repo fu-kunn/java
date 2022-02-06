80b021d5df64a734d77ae989060947127b698ffe
EC2 Default User <ec2-user@ip-172-31-36-77.ec2.internal>
2022-02-06 15:14:50 +0000

    [Finish]dot9章

--- MyApp.java
+++ MyApp.java
@@ -1,8 +1,23 @@
 public class MyApp {
 
  public static void main(String[] args) {
-  String msg = "Hello World Again!";
 
-  System.out.println(msg);
+ String signal = "green";
+ switch (signal) {
+  case "red":
+   System.out.println("Stop!");
+   break;
+  case "green":
+  case "blue":
+   System.out.println("go!");
+   break;
+  case "ryellow":
+   System.out.println("caution!");
+   break;
+  default:
+   System.out.println("wrong signal!");
+   break;
+ }
+
 }
 }
