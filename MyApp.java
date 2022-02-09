192d70e7cabdbfadb7b2f78bd252912b604a0124
EC2 Default User <ec2-user@ip-172-31-36-77.ec2.internal>
2022-02-09 05:21:45 +0000

    [Finish]14章

--- MyApp.java
+++ MyApp.java
@@ -2,23 +2,11 @@ public class MyApp {
 
  public static void main(String[] args) {
 
- String signal = "green";
- switch (signal) {
-  case "red":
-   System.out.println("Stop!");
-   break;
-  case "green":
-  case "blue":
-   System.out.println("go!");
-   break;
-  case "ryellow":
-   System.out.println("caution!");
-   break;
-  default:
-   System.out.println("wrong signal!");
-   break;
- }
+ int [] sales = {700, 400, 500};
 
+ for (int sale : sales) {
+  System.out.println(sale);
+ }
 
 }
 }
