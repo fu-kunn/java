023ab436007209447644cdf8b57e516b960cd1b9
EC2 Default User <ec2-user@ip-172-31-36-77.ec2.internal>
2022-04-14 09:44:45 +0000

    [数当てゲーム]

--- MyApp.java
+++ MyApp.java
@@ -1,12 +1,17 @@
 public class MyApp {
-
- public static void main(String[] args) {
-
- int [] sales = {700, 400, 500};
-
- for (int sale : sales) {
-  System.out.println(sale);
- }
-
-}
+  public static void main(String[]  args) {
+    System.out.print("[数当てゲーム]");
+    int ans = new java.util.Random().nextInt(10);
+    for (int i = 0; i < 5; i++) {
+      System.out.println("0~9の数字を入力してください");
+      int num = new java.util.Scanner(System.in).nextInt();
+      if (ans == num) {
+        System.out.println("あたり！");
+        break;
+      } else {
+        System.out.println("違います");
+      }
+    }
+    System.out.println("ゲームを終了します");
+  }
 }
