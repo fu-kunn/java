88d67bce5a8e93ca45d6bf33c603d94c2fab745f
fu-kunn <sky53908@gmail.com>
2022-04-19 13:18:51 +0000

    [8-13]

---
+++ Kamisama.java
@@ -0,0 +1,12 @@
+public class Kamisama {
+  public static void main(String[] args) {
+    Hero h = new Hero();
+    h.name = "ミナト";
+    h.hp = 100;
+    System.out.println("勇者" + h.name + "を生み出したました");
+    h.sit(5);
+    h.slip();
+    h.sit(25);
+    h.run();
+  }
+}
