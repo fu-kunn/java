4cfd3a114457461f9f2daab5ed4407656661340f
fu-kunn <sky53908@gmail.com>
2022-04-19 13:28:01 +0000

    [

--- Kamisama.java
+++ Kamisama.java
@@ -3,10 +3,20 @@ public class Kamisama {
     Hero h = new Hero();
     h.name = "ミナト";
     h.hp = 100;
-    System.out.println("勇者" + h.name + "を生み出したました");
-    h.sit(5);
+
+    Matango m1 = new Matango();
+    m1.hp = 50;
+    m1.suffix = 'A';
+    Matango m2 = new Matango();
+    m2.hp = 48;
+    m2.suffix = 'B';
+
+    // System.out.println("勇者" + h.name + "を生み出したました");
+
+    // 冒険の始まり
     h.slip();
-    h.sit(25);
+    m1.run();
+    m2.run();
     h.run();
   }
 }
