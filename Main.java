6ba1d54f22009dec332bf3e3ec1718b272e4b0b7
fu-kunn <sky53908@gmail.com>
2022-04-20 14:47:25 +0000

    [9-13]

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
-import comment.Zenhan;
 public class Main {
-  public static void main(String[] args) throws Exception {
-    Zenhan.doWarusa();
-    Zenhan.doTogame();
-    comment.Kouhan.callDeae();
-    comment.Kouhan.showMondokoro();
+  public static void main(String[] args) {
+    Hero h1 = new Hero("ミナト");
+    System.out.println(h1.name);
+    Hero h2 = new Hero();
+    System.out.println(h2.name);
   }
 }
+
+
+// public class Main {
+//   public static void main(String[] args) {
+//     Sword s = new Sword();
+//     s.name = "炎の剣";
+//     s.damage = 10;
+//     Hero h = new Hero();
+//     h.name = "ミナト";
+//     h.hp = 100;
+//     h.sword = s;
+//     System.out.println("現在の武器は" + h.sword.name);
+//   }
+// }
