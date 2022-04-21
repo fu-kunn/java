dc56342967393d42636c774a9f8aa296b56fab81
fu-kunn <sky53908@gmail.com>
2022-04-21 12:10:38 +0000

    [10-6]

--- Main.java
+++ Main.java
@@ -1,22 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    Hero h1 = new Hero("ミナト");
-    System.out.println(h1.name);
-    Hero h2 = new Hero();
-    System.out.println(h2.name);
+    Hero h = new Hero();
+    h.run();
+    SuperHero sh = new SuperHero();
+    sh.run();
   }
 }
-
-
-// public class Main {
-//   public static void main(String[] args) {
-//     Sword s = new Sword();
-//     s.name = "炎の剣";
-//     s.damage = 10;
-//     Hero h = new Hero();
-//     h.name = "ミナト";
-//     h.hp = 100;
-//     h.sword = s;
-//     System.out.println("現在の武器は" + h.sword.name);
-//   }
-// }
