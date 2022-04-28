522b239b1105e6afed38b743b9f391cd71be43d0
fu-kunn <sky53908@gmail.com>
2022-04-28 05:48:29 +0000

    [練習12−３]

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main {
   public static void main(String[] args) {
-    Hero h = new Hero();
-    h.run();
-    SuperHero sh = new SuperHero();
-    sh.run();
+    Y[] array = new Y[2];
+    array[0] = new A();
+    array[1] = new B();
+    for (Y y : array) {
+      y.b();
+    }
   }
 }
