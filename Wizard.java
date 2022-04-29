48817f9bd5295a38f4b36badf314b1c1c0493af2
fu-kunn <sky53908@gmail.com>
2022-04-29 11:31:07 +0000

    [練習13-1]

---
+++ Wizard.java
@@ -0,0 +1,12 @@
+public class Wizard {
+ private int hp;
+ private int mp;
+ private String name;
+ private Wand wand;
+ public void heal(Hero h) {
+   int basePoint = 10;
+   int recovPoimt = (int)(basePoint * this.wand.power);
+   h.setHp(h.getHp() + recovPoint);
+   System.out.println(h.getName() + "のHPを" recovPoint + "回復した！");
+ }
+}
