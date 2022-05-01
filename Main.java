708195b69edb490ed9aa991b28d93a08fdfa138d
fu-kunn <sky53908@gmail.com>
2022-05-01 11:46:21 +0000

    [練習15-1]

--- Main.java
+++ Main.java
@@ -1,16 +1,10 @@
-import java.util.Calendar;
-import java.util.Date;
-
 public class Main {
   public static void main(String[] args) {
-    Calendar c = Calendar.getInstance();
-    c.set(2019,8,22,1,23,45);
-    c.set(Calendar.MONTH, 9);
-    Date d = c.getTime();
-    System.out.println(d);
-    Date now = new Date();
-    c.setTime(now);
-    int y = c.get(Calendar.YEAR);
-    System.out.println("今年は" + y + "年です");
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 100; i++) {
+      sb.append(i+1).append(",");
+    }
+    String s = sb.toString();
+    String[] a = s.split(",");
   }
 }
