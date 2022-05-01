4f2aa6256b8d19d5aa94213fa34a32b5fdc7403a
fu-kunn <sky53908@gmail.com>
2022-05-01 12:02:46 +0000

    [練習15-4]

--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Main {
   public static void main(String[] args) {
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < 100; i++) {
-      sb.append(i+1).append(",");
-    }
-    String s = sb.toString();
-    String[] a = s.split(",");
+    Date now = new Date();
+    Calendar c = Calendar.getInstance();
+    c.setTime(now);
+    int day = c.get(Calendar.DAY_OF_MONTH);
+    day += 100;
+    c.set(Calendar.DAY_OF_MONTH, day);
+    Date future = c.getTime();
+    SimpleDateFormat f = new SimpleDateFormat("西暦yyyy年MM月dd日");
+    System.out.println(f.format(future));
   }
 }
