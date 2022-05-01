96fc47e25b94624b10238ebefeffc0610d69a6f0
fu-kunn <sky53908@gmail.com>
2022-05-01 08:54:58 +0000

    [コード　15−１]

--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
 public class Main {
   public static void main(String[] args) {
-    Y[] array = new Y[2];
-    array[0] = new A();
-    array[1] = new B();
-    for (Y y : array) {
-      y.b();
+    String s1 = "スッキリJava";
+    String s2 = "Java";
+    String s3 = "java";
+    if (s2.equals(s3)) {
+      System.out.println("s2とs3は等しい");
     }
+    if (s2.equalsIgnoreCase(s3)) {
+      System.out.println("s2とs3はケースを区別しなければ等しい");
+    }
+    System.out.println("s1の長さは" + s1.length() + "です");
+     if (s1.isEmpty()) {
+       System.out.println("s1は空文字です");
+     }
   }
 }
