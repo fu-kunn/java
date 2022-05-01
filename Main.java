4fc4c073f2580d2928b7e08dd9ca32ecc1fab693
fu-kunn <sky53908@gmail.com>
2022-05-01 11:09:50 +0000

    [コード 15-11]現在日時の表示

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
+import java.util.Date;
+
 public class Main {
   public static void main(String[] args) {
-    String s = "abc,def:ghi";
-    String[] words = s.split("[,:]");
-    for (String w : words) {
-      System.out.println(w + "->");
-    }
+    Date now = new Date();
+    System.out.println(now);
+    System.out.println(now.getTime());
+    Date past = new Date(1600705425827L);
+    System.out.println(past);
   }
 }
