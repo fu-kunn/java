391ccea1b13daa3511dc4054be396afc9440b867
fu-kunn <sky53908@gmail.com>
2022-05-02 05:42:43 +0000

    [コード 16-5]TreeSet

--- Main.java
+++ Main.java
@@ -1,17 +1,15 @@
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
   public static void main(String[] args) {
-    Date now = new Date();
-    Calendar c = Calendar.getInstance();
-    c.setTime(now);
-    int day = c.get(Calendar.DAY_OF_MONTH);
-    day += 100;
-    c.set(Calendar.DAY_OF_MONTH, day);
-    Date future = c.getTime();
-    SimpleDateFormat f = new SimpleDateFormat("西暦yyyy年MM月dd日");
-    System.out.println(f.format(future));
+    Set<String> words = new TreeSet<String>();
+    words.add("dog");
+    words.add("cat");
+    words.add("wolf");
+    words.add("panda");
+    for (String s : words) {
+      System.out.print(s + "→");
+    }
   }
 }
